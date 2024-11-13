762f9c72c8a840ae979758aa14a498bf97c21f19
Artur Shiriev <lesnik512@gmail.com>
2024-11-13 12:53:47 +0300

    inherit InjectedFactory from AbstractProvider

--- packages/modern-di/modern_di/providers/injected_factory.py
+++ packages/modern-di/modern_di/providers/injected_factory.py
@@ -9,16 +9,21 @@ T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
 
 
-class InjectedFactory(typing.Generic[T_co]):
-    __slots__ = ("_factory_provider",)
+class InjectedFactory(AbstractProvider[T_co]):
+    __slots__ = [*AbstractProvider.BASE_SLOTS, "_factory_provider"]
 
     def __init__(self, factory_provider: AbstractProvider[T_co]) -> None:
+        super().__init__(factory_provider.scope)
         self._factory_provider = factory_provider
 
-    async def async_resolve(self, container: Container) -> typing.Callable[[], T_co]:
+    async def async_resolve(self, container: Container) -> typing.Callable[[], T_co]:  # type: ignore[override]
         await self._factory_provider.async_resolve(container)
         return functools.partial(self._factory_provider.sync_resolve, container)
 
-    def sync_resolve(self, container: Container) -> typing.Callable[[], T_co]:
+    def sync_resolve(self, container: Container) -> typing.Callable[[], T_co]:  # type: ignore[override]
         self._factory_provider.sync_resolve(container)
         return functools.partial(self._factory_provider.sync_resolve, container)
+
+    @property
+    def cast(self) -> typing.Callable[[], T_co]:  # type: ignore[override]
+        return typing.cast(typing.Callable[[], T_co], self)
--- packages/modern-di/tests_core/providers/test_injected_factory.py
+++ packages/modern-di/tests_core/providers/test_injected_factory.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime
+import typing
 
 import pytest
 from modern_di import Container, Scope, providers
@@ -12,10 +13,21 @@ class DependentCreator:
     dep1: datetime.datetime
 
 
+def factory_with_injected_factory(factory: typing.Callable[[], DependentCreator]) -> None:
+    instance1 = factory()
+    instance2 = factory()
+    assert instance1 is not instance2
+    assert isinstance(instance1, DependentCreator)
+    assert isinstance(instance2, DependentCreator)
+
+
 async_resource = providers.Resource(Scope.APP, create_async_resource)
 sync_resource = providers.Resource(Scope.APP, create_sync_resource)
 request_sync_factory = providers.Factory(Scope.REQUEST, DependentCreator, dep1=sync_resource.cast)
 request_async_factory = providers.Factory(Scope.REQUEST, DependentCreator, dep1=async_resource.cast)
+factory_with_injected_factory_dep = providers.Factory(
+    Scope.REQUEST, factory_with_injected_factory, factory=request_sync_factory.factory_provider.cast
+)
 
 
 async def test_injected_async_factory() -> None:
@@ -40,6 +52,14 @@ async def test_injected_sync_factory() -> None:
         assert instance1 is not instance2
         assert isinstance(instance1, DependentCreator)
         assert isinstance(instance2, DependentCreator)
+
+
+async def test_injected_factory_inside_factory() -> None:
+    async with (
+        Container(scope=Scope.APP) as app_container,
+        app_container.build_child_container(scope=Scope.REQUEST) as request_container,
+    ):
+        await factory_with_injected_factory_dep.async_resolve(request_container)
 
 
 async def test_injected_async_factory_in_sync_mode() -> None:
