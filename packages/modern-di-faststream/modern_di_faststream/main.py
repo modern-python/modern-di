11208f153693d84d05e5ef24fb2ad745383e83ef
Artur Shiriev <lesnik512@gmail.com>
2025-03-01 12:11:35 +0300

    refactor

--- packages/modern-di-faststream/modern_di_faststream/main.py
+++ packages/modern-di-faststream/modern_di_faststream/main.py
@@ -1,38 +1,35 @@
 import dataclasses
 import enum
-import importlib
 import typing
 from collections.abc import Awaitable, Callable
+from importlib.metadata import version
 
 import faststream
 import modern_di
-from faststream import BaseMiddleware, ContextRepo
 from faststream.broker.message import StreamMessage
 from faststream.types import DecodedMessage
 from modern_di import Container, Scope, providers
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
+P = typing.ParamSpec("P")
 
-FASTSTREAM_VERSION = importlib.metadata.version("faststream")
-major, minor, *_ = FASTSTREAM_VERSION.split(".")
-_FASTSTREAM_MAJOR, _FASTSTREAM_MINOR = int(major), int(minor)
+_major, _minor, *_ = version("faststream").split(".")
+_OLD_MIDDLEWARES = int(_major) == 0 and int(_minor) < 6  # noqa: PLR2004
 
 
-class DIMiddleware:
+class _DIMiddlewareFactory:
     __slots__ = ("di_container",)
 
     def __init__(self, di_container: Container) -> None:
         self.di_container = di_container
 
-    # Consumes args, kwargs due it has no matter for modern-di logic
-    # and allows to be compatible with future FastStreamd middlewares signature
-    def __call__(self, *args: typing.Any, **kwargs: typing.Any) -> "_DiMiddleware":  # noqa: ANN401
-        return _DiMiddleware(*args, di_container=self.di_container, **kwargs)
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> "_DiMiddleware[P]":
+        return _DiMiddleware(self.di_container, *args, **kwargs)
 
 
-class _DiMiddleware(BaseMiddleware):
-    def __init__(self, *args: typing.Any, di_container: Container, **kwargs: typing.Any) -> None:  # noqa: ANN401
+class _DiMiddleware(faststream.BaseMiddleware, typing.Generic[P]):
+    def __init__(self, di_container: Container, *args: P.args, **kwargs: P.kwargs) -> None:
         self.di_container = di_container
         super().__init__(*args, **kwargs)
 
@@ -50,26 +47,28 @@ class _DiMiddleware(BaseMiddleware):
                     await call_next(msg),
                 )
 
-    if _FASTSTREAM_MAJOR == 0 and _FASTSTREAM_MINOR < 6:  # noqa: PLR2004
+    if _OLD_MIDDLEWARES:  # pragma: no cover
 
         @property
-        def faststream_context(self) -> ContextRepo:
+        def faststream_context(self) -> faststream.ContextRepo:
             return faststream.context
 
-    else:
+    else:  # pragma: no cover
 
         @property
-        def faststream_context(self) -> ContextRepo:
+        def faststream_context(self) -> faststream.ContextRepo:
             return self.context  # type: ignore[attr-defined,no-any-return]
 
 
 def setup_di(app: faststream.FastStream, scope: enum.IntEnum = Scope.APP) -> Container:
-    assert app.broker, "broker must be defined to setup DI"
+    if not app.broker:
+        msg = "Broker must be defined to setup DI"
+        raise RuntimeError(msg)
 
     container = Container(scope=scope)
     app.on_startup(container.__aenter__)
     app.after_shutdown(container.async_close)
-    app.broker.add_middleware(DIMiddleware(container))
+    app.broker.add_middleware(_DIMiddlewareFactory(container))
     return container
 
 
@@ -77,7 +76,7 @@ def setup_di(app: faststream.FastStream, scope: enum.IntEnum = Scope.APP) -> Con
 class Dependency(typing.Generic[T_co]):
     dependency: providers.AbstractProvider[T_co]
 
-    async def __call__(self, context: ContextRepo) -> T_co:
+    async def __call__(self, context: faststream.ContextRepo) -> T_co:
         request_container: modern_di.Container = context.get("request_container")
         return await self.dependency.async_resolve(request_container)
 
