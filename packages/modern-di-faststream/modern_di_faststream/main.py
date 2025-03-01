03eb9b4865ef5bad3c242969a0a765d00354121c
Nikita Pastukhov <diementros@yandex.ru>
2025-03-01 11:28:51 +0300

    refactor: support FastStream 0.5 and 0.6 versions both

--- packages/modern-di-faststream/modern_di_faststream/main.py
+++ packages/modern-di-faststream/modern_di_faststream/main.py
@@ -1,11 +1,11 @@
 import dataclasses
 import enum
+import importlib
 import typing
 from collections.abc import Awaitable, Callable
 
 import faststream
 import modern_di
-import typing_extensions
 from faststream import BaseMiddleware, ContextRepo
 from faststream.broker.message import StreamMessage
 from faststream.types import DecodedMessage
@@ -14,14 +14,27 @@ from modern_di import Container, Scope, providers
 
 T_co = typing.TypeVar("T_co", covariant=True)
 
+FASTSTREAM_VERSION = importlib.metadata.version("faststream")
+major, minor, *_ = FASTSTREAM_VERSION.split(".")
+_FASTSTREAM_MAJOR, _FASTSTREAM_MINOR = int(major), int(minor)
 
-class DIMiddleware(BaseMiddleware):
+
+class DIMiddleware:
+    __slots__ = ("di_container",)
+
     def __init__(self, di_container: Container) -> None:
         self.di_container = di_container
 
-    def __call__(self, msg: typing.Any | None = None) -> typing_extensions.Self:  # noqa: ANN401
-        self.msg = msg
-        return self
+    # Consumes args, kwargs due it has no matter for modern-di logic
+    # and allows to be compatible with future FastStreamd middlewares signature
+    def __call__(self, *args: typing.Any, **kwargs: typing.Any) -> "_DiMiddleware":  # noqa: ANN401
+        return _DiMiddleware(*args, di_container=self.di_container, **kwargs)
+
+
+class _DiMiddleware(BaseMiddleware):
+    def __init__(self, *args: typing.Any, di_container: Container, **kwargs: typing.Any) -> None:  # noqa: ANN401
+        self.di_container = di_container
+        super().__init__(*args, **kwargs)
 
     async def consume_scope(
         self,
@@ -31,11 +44,23 @@ class DIMiddleware(BaseMiddleware):
         async with self.di_container.build_child_container(
             scope=modern_di.Scope.REQUEST, context={"message": StreamMessage[typing.Any]}
         ) as request_container:
-            with faststream.context.scope("request_container", request_container):
+            with self.faststream_context.scope("request_container", request_container):
                 return typing.cast(
                     typing.AsyncIterator[DecodedMessage],
                     await call_next(msg),
                 )
+
+    if _FASTSTREAM_MAJOR == 0 and _FASTSTREAM_MINOR < 6:  # noqa: PLR2004
+
+        @property
+        def faststream_context(self) -> ContextRepo:
+            return faststream.context
+
+    else:
+
+        @property
+        def faststream_context(self) -> ContextRepo:
+            return self.context  # type: ignore[attr-defined,no-any-return]
 
 
 def setup_di(app: faststream.FastStream, scope: enum.IntEnum = Scope.APP) -> Container:
