2cb03138db2cf695cf0b73e46e7cae7762001d5e
Artur Shiriev <lesnik512@gmail.com>
2026-06-26 09:03:16 +0300

    feat: add Group.get_named_providers()

--- modern_di/group.py
+++ modern_di/group.py
@@ -13,6 +13,21 @@ class Group:
         raise exceptions.GroupInstantiationError(group_name=cls.__name__)
 
     @classmethod
+    def get_named_providers(cls) -> dict[str, AbstractProvider[typing.Any]]:
+        seen_names: set[str] = set()
+        collected: dict[str, AbstractProvider[typing.Any]] = {}
+        for klass in cls.__mro__:
+            if klass is Group or klass is object:
+                continue
+            for name, value in klass.__dict__.items():
+                if name in seen_names:
+                    continue
+                seen_names.add(name)
+                if isinstance(value, AbstractProvider):
+                    collected[name] = value
+        return collected
+
+    @classmethod
     def get_providers(cls) -> list[AbstractProvider[typing.Any]]:
         seen_names: set[str] = set()
         collected: list[AbstractProvider[typing.Any]] = []
